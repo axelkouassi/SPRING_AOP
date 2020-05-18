3db5014103c8b00b1a864d8b2756b918217df756
Axel Kouassi <ak659@njit.edu>
2020-05-17 23:41:48 -0400

    @Pointcuts AOP

--- Spring_AOP/src/com/axel/aspect/LoggingAspect.java
+++ Spring_AOP/src/com/axel/aspect/LoggingAspect.java
@@ -2,13 +2,22 @@ package com.axel.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice Run. Get Method called.");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second Advice executed.");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
 
 }
