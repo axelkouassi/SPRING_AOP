58226c80f6eeaa838e2e06ce6413428764f65a23
Axel Kouassi <ak659@njit.edu>
2020-05-17 22:05:43 -0400

    AOP Advice

---
+++ Spring_AOP/src/com/axel/AOPMain.java
@@ -0,0 +1,17 @@
+package com.axel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.axel.service.ShapeService;
+
+public class AOPMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		System.out.println(shapeService.getCircle().getName());
+
+	}
+
+}
