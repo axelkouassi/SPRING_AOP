74f987334c238c862b0b959901c33dfc3f88ecdd
Axel Kouassi <ak659@njit.edu>
2020-05-19 00:05:29 -0400

    Around Advice types

--- Spring_AOP/src/com/axel/AOPMain.java
+++ Spring_AOP/src/com/axel/AOPMain.java
@@ -11,8 +11,9 @@ public class AOPMain {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
 		//System.out.println(shapeService.getTriangle().getName());
-		shapeService.getCircle().setName("Dummy name");
+		//shapeService.getCircle().setName("Dummy name");
 		//System.out.println(shapeService.getCircle().getName());
+		shapeService.getCircle();
 
 	}
 
--- Spring_AOP/src/com/axel/aspect/LoggingAspect.java
+++ Spring_AOP/src/com/axel/aspect/LoggingAspect.java
@@ -1,9 +1,11 @@
 package com.axel.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -32,6 +34,23 @@ public class LoggingAspect {
 	@AfterThrowing(pointcut="args(name)", throwing="ex")
 	public void exceptionAdvice(String name, RuntimeException ex) {
 		System.out.println("An exception " + ex + " has been thrown.");
+	}
+	
+	@Around("allGetters()")
+	public Object myAroundAdvice(ProceedingJoinPoint proceedingJointPoint) {
+		
+		Object returnValue = null;
+		try {
+			System.out.println("Before Advice");
+			returnValue = proceedingJointPoint.proceed();
+			System.out.println("After Returning");
+		} catch (Throwable e) {
+			System.out.println("After throwing");;
+		}
+		
+		System.out.println("After Finally");
+		
+		return returnValue;
 	}
 	
 	@Pointcut("execution(* get*())")
